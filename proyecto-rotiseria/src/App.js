705587df1ccefee932f308cfe1b92e9c6b46c572
Mauricio Roldán <roldanmauriciomax@gmail.com>
2023-05-02 22:23:31 -0300

    inicio del proyecto

---
+++ proyecto-rotiseria/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
+
+// componente NAV <- FRANCO
+// componente MENU / Promo
+// componente ABOUT
+// componente producto <- MAURI
+// componente CONTACTO <- LEO
+// componente CATEGORIA
+// componente COMPRA
+// formulario LOGIN
+// agregar producto
+// eliminar producto
+// cambiar precio
+// PUNTOS (opcional)
+// canjear puntos (opcional)
