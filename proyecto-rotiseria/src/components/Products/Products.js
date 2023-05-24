d08374ee5f17b1ee36e72450d1175a5bca1e8c29
Mauricio Roldán <roldanmauriciomax@gmail.com>
2023-05-23 23:20:43 -0300

    Update Menú/Products with JSON

--- proyecto-rotiseria/src/components/Products/Products.js
+++ proyecto-rotiseria/src/components/Products/Products.js
@@ -1,56 +1,40 @@
-import React from 'react'
-import ErrorPage from '../../Pages/ErrorPage';
-const idGenerate = () => {
-    const values = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    let password = "";
-    for (let x = 0; x < 10; x++) {
-        let aleatory = Math.floor(Math.random() * values.length);
-        password += values.charAt(aleatory);
-    }
-    return password
-}
-let filteredInventory = [];
-const inventory = [
-    {
-        Type: "Burger",
-        Name: "CheeseBurger",
-        Id: idGenerate(),
-        Price: 1500,
-        Ingredients: ["Doble Carne", "Cheedar", "Tomate", "Cebolla", "Lechuga", "Aderezos"]
-    },
-    {
-        Type: "Burger",
-        Name: "KingBurger",
-        Id: idGenerate(),
-        Price: 1500,
-        Ingredients: ["Doble Carne", "Extra Cheedar", "Tomate", "Cebolla caramelizada", "Lechuga", "Aderezos"]
-    },
-    {
-        Type: "Pizza",
-        Name: "Doble Muzza",
-        Id: idGenerate(),
-        Price: 1400,
-        Ingredients: ["queso", "tomate"]
-    }
-]
+import React, { useEffect, useState } from 'react'
+import products from './products.json'
+const Products = () => {
+        const [filteredProducts, setFilteredProducts] = useState([])
+        const unicos = [];
+        for(var i = 0; i < products.length; i++) {
+        
+            const elemento = products[i].Type;
+        
+            if (!unicos.includes(products[i].Type)) {
+            unicos.push(elemento);
+        }
 
-const Products = ({Typo}) => {
-    filteredInventory = [];
-    return (
+        }
+        const selectedProduct = (type) => {
+            setFilteredProducts([])
+            setFilteredProducts(products.filter(p => p.Type===type))
+            console.log(type)
+            return filteredProducts
+        }
+        const setState =()=>(setFilteredProducts([...filteredProducts, 1]))
+        return (
         <div>
-            {inventory.map((item) => {if (item.Type==Typo) {
-                filteredInventory.push(item)
-            }})}
-            {filteredInventory.map((item) => (<ul key={item.Id}>
-                <li>{item.Name}</li>
-                <li>precio: ${item.Price}</li>
-                <li>ID:{item.Id}</li>
-                <li>Ingredientes:{item.Ingredients.map((item) => (
-                    <ul key={idGenerate()} id='ingredientes' style={{marginLeft:25}}>
-                        <li>{item}</li>
-                    </ul>))}
+            {unicos.map((item,index)=><button key={index} onClick={()=>selectedProduct(item)}>{item}</button>)}
+            <button onClick={setState}>setter</button>
+            {filteredProducts.map((item, index)=>(<ul key={item.Id}>
+                <li><b>{item.Name}</b></li>
+                <li><b>Precio:</b>{item.Price}</li>
+                <li><b>ID:</b>{item.Id}</li>
+                <li>
+                    <b>Ingredientes:</b>{item.Ingredients.map((item, index)=>(
+                        <ul key={index}>
+                            <li>{item}</li>
+                        </ul>
+                    ))}
                 </li>
-                <hr />
+                <hr/>
             </ul>))}
         </div>
     )
