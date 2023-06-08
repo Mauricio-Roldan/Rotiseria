a4374c0c5ea3fa7fb7de75b7aefee282987150d5
LeonelZarini <leonelzarini@hotmail.com>
2023-06-08 19:22:19 -0300

    AboutUs

--- proyecto-rotiseria/src/App.js
+++ proyecto-rotiseria/src/App.js
@@ -2,7 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 import Products from './components/Products/Products';
 import Navbar from './components/Navbar/NavBar';
-
 function App() {
   return (
     <div className="App">
